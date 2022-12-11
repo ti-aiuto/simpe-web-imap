36e4a12a18f47e647b715900013a1a7a642a0df1
ti-aiuto <ti.aiuto@outlook.jp>
2022-12-12 01:00:12 +0900

    :pencil2: 実装変更

--- server/server.rb
+++ server/server.rb
@@ -13,33 +13,36 @@ imap_password = ENV['SIMPLE_WEB_IMAP_IMAP_PASSWORD']
 imap_usessl = true
 
 imap = Net::IMAP.new(imap_host, imap_port, imap_usessl)
-imap.login(imap_user, imap_password)
+imap.login(imap_username, imap_password)
 
 imap.examine('INBOX')
 
-search_criterias = ["SINCE", Net::IMAP.format_date(Date.new(2022, 12, 1))]
+search_criterias = ["SINCE", Net::IMAP.format_date(Date.new(2022, 12, 10))]
+message_ids = imap.search(search_criterias)
+message_ids.each_slice(100) do |message_ids|
+    p "API呼び出し"
+    messages = imap.fetch(message_ids, ["RFC822", "FLAGS"])
+    messages.each do |message|
+        mail = Mail.new(message.attr["RFC822"])
+        p mail.subject
+        p mail.from
+        p mail.date.to_s
+        p message.attr["FLAGS"].include?(:Seen)
 
-subject_attr_name = 'BODY[HEADER.FIELDS (SUBJECT)]'
-body_attr_name = 'BODY[TEXT]'
+        # # multipartなメールかチェック
+        # if m.multipart?
+        #   # plantextなメールかチェック
+        #   if m.text_part
+        #     body = m.text_part.decoded
+        #   # htmlなメールかチェック
+        #   elsif m.html_part
+        #     body = m.html_part.decoded
+        #   end
+        # else
+        #   body = m.body
+        # end
 
-imap.search(search_criterias, "RFC822").each do |msg_id|
-    messages = imap.fetch(msg_id, [subject_attr_name, body_attr_name])
-    p messages
-
-    message = messages.first
-
-    m = Mail.new(message.attr["RFC822"])
-    # multipartなメールかチェック
-    if m.multipart?
-      # plantextなメールかチェック
-      if m.text_part
-        body = m.text_part.decoded
-      # htmlなメールかチェック
-      elsif m.html_part
-        body = m.html_part.decoded
-      end
-    else
-      body = m.body
+        # imap.store(msg_id, '+FLAGS', :Seen)
     end
 end
 
